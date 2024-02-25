2cc79c502a54d167d1b17cc275f73dcb971b17f8
Ramanathan <uraman@vidhvatthamah.com>
2024-02-25 21:21:20 +0530

    Update wishlist.controller.js
    Added wishlist

--- backend/controller/wishlist.controller.js
+++ backend/controller/wishlist.controller.js
@@ -9,11 +9,27 @@ const sequelize = require('../config/dbconfig');
 const Wishlist = require('../models/wishlist')
 const { request } = require('http');
 
-//const app = express.Router();
-const app = express();
+const app = express.Router();
 app.use(cors());
 app.use(express.json());
 
+
+//view the wishlist
+app.get('/wishlist',async(req,res)=>{
+  try{
+    const allWish = await Wishlist.findAll();
+    //console.log(allWish);
+    res.status(200).json({
+        message : "ok",
+        payload : allWish
+    });
+}
+catch (err){
+    res.status(400).json({
+        message:err
+    })
+}
+});
 
 //insert into wishlist
 app.post('/addwishlist',async(req,res)=>{
@@ -22,7 +38,8 @@ app.post('/addwishlist',async(req,res)=>{
             _id:crypto.randomUUID(),
             Book_id:req.body.Book_id,
             Customer_id:req.body.Customer_id,
-            inCart:0
+            inCart:0,
+            cartQuantity:0
         }
         const newWishList = Wishlist.create(wishListData);
         console.log(newWishList);
@@ -37,19 +54,46 @@ app.post('/addwishlist',async(req,res)=>{
 });
 
 //delete from wishlist
-app.post('/deletewishlist', async(req, res)=>{
-    Wishlist.findByPk(_id=req.body.id).then(function(Wishlist){
-      return Wishlist.destroy();
-    }).then(function(){
-        if(Wishlist){
-            res.send("done")
-        } else {
-            res.status(400).send('Error in deleting record')
-        }
-    })
+app.delete('/deletewishlist', async(req, res)=>{
+  await Wishlist.destroy({
+    where: {
+       Book_id:req.body.Book_id , Customer_id:req.body.Customer_id 
+    }
+  }).then(function(){
+    if(Wishlist){
+        res.status(200).send("done")
+    } else {
+        res.status(400).send('Error in deleting record')
+    }
+})
   });
 
-///module.exports = app;
-app.listen(8080 , ()=>{
-    console.log(`app running on port 8080`);
-})
+
+  //change item from wishlist to cart
+app.post('/addtocart',async(req,res)=>{
+  try{
+      const Book_id=req.body.Book_id;
+      const Customer_id = req.body.Customer_id;
+      const updateBook = await Wishlist.findOne({ where: { Book_id:Book_id , Customer_id:Customer_id } });
+      const primaryKey = updateBook._id;
+      const updateCart = {
+        Book_id:Book_id,
+        Customer_id:Customer_id,
+        inCart:1,
+        cartQuantity:1
+    };  
+    const result = await Wishlist.update(updateCart, {
+        where: { _id: primaryKey } // Update the entry with the given id
+    });  
+    console.log(result);
+        res.status(200).json({
+            message :"Success"
+        })
+  }catch(err){
+    res.status(400).json({
+      message:err
+    })
+  }
+});
+
+module.exports = app;
